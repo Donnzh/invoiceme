d361431856e787231bc734e7a214d8b79862ba2f
DonnZh <dongyang.tcm@gmail.com>
2019-04-28 13:50:15 +1000

    Updates

---
+++ precache-manifest.cfaf58993b91970ba9d118578c7b271d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "67762b9d648b289d643c",
+    "url": "/invoiceme/static/js/runtime~main.91b20e75.js"
+  },
+  {
+    "revision": "a73c49e42285d38c26ce",
+    "url": "/invoiceme/static/js/main.261d5c4c.chunk.js"
+  },
+  {
+    "revision": "a2e8c23af5a04bd24fe5",
+    "url": "/invoiceme/static/js/2.35279fb3.chunk.js"
+  },
+  {
+    "revision": "a4507c6b36b62b5563246cf582b810bc",
+    "url": "/invoiceme/index.html"
+  }
+];
